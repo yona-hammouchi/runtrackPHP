4cb28d1924ebe8c48fe91762c9746ced78fa2df5
yona-hammouchi <yona.hammouchi@laplateforme.io>
2025-01-14 13:46:39 +0100

    finish job 02

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,3 +1,31 @@
 <?php
 $str = "LaPlateforme";
 echo $str;
+?>
+
+
+<?php
+$str2 = "Vive";
+$str3 = "!";
+?>
+
+<?php
+// concaténant l’ensemble des variables
+echo $str2 . " LaPlateforme " . $str3 . "<br>";
+
+$val = 6;
+echo $val . "<br>";
+
+$val += 4;
+echo $val . "<br>";
+?>
+
+<?php
+$myBool = true;
+echo $myBool ? 'true' : 'false' . "<br>";
+
+$myBool = false;
+echo $myBool ? 'true' : 'false';
+
+?>
+
