515e282b9870c73bebece25afec2239d3d2f60aa
yona-hammouchi <yona.hammouchi@laplateforme.io>
2025-01-14 14:12:46 +0100

    add folder job03

---
+++ job03/index.php
@@ -0,0 +1,3 @@
+$str = "LaPlateforme";
+
+$a = 5;
---
+++ jour01/job02/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "Hello LaPlateforme!"; // This is a single-line comment
+echo "Hello LaPlateforme!"; # This is also a single-line comment
